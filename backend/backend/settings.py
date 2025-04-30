525db9b86344aa9afef22bfe750ccc4986ecde36
Ankit Kumar <ankitkumar786687@gmail.com>
2025-05-01 02:19:06 +0530

    fix: switched to dj_database_url for Render PostgreSQL

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -88,14 +89,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'skillswap',
-        'USER': 'postgres',
-        'PASSWORD': 'Banku*143',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
